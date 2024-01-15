8f68f145f4cf725e134ba84fb344d5c5a8e26179
Wesner-Software-Ausbildung <sergei.pron@wesner-software.de>
2024-01-15 09:15:08 +0100

    zuul 0

---
+++ worldofzuul/src/de/szut/zuul/ZuulUI.java
@@ -0,0 +1,16 @@
+package de.szut.zuul;
+
+public class ZuulUI {
+
+    /**
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+}
+
+
